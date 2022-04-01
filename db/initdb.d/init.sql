e343cd5fc318e44fdb1697cd516f4e272ccc3fb9
roy29fuku <roy29fuku@gmail.com>
2022-04-01 15:47:07 +0900

    add sample data

---
+++ db/initdb.d/init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS sample;
+CREATE DATABASE sample;
+USE sample;
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
